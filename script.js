135e27d6831ea852e92f11d372a8e6cb62a87da2
willianleal-222 <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>
2025-06-16 18:58:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Arrays (Metodos)
+
+//let cart = ['Agua', 'Arroz', 'Carne', 'Feijao']
+//let numbers = [10, 20, 33, 40, 5, 13, true]
+
+//console.log(cart)
+
+let nome = 'Willian Leal'
+console.log('O nome informado e ' + nome)
+
